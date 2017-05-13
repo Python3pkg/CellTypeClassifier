6f48dad6b78a0bd994744a66cf2453b2538653c2
maximebeau <maximebeaujeanroch047@gmail.com>
2017-05-13 23:53:46 +0100

    +

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-setup.py file
+from distutils.core import setup
+setup(
+  name = 'CellTypeCLassifier',
+  packages = ['CellTypeCLassifier'], # this must be the same as the name above
+  version = 'beta',
+  description = 'Kilosort/phy generated units supervised and unsupervised classification, with the hope to sort them by cell type.',
+  author = 'Maxime Beau',
+  author_email = 'm.beau047@gmail.com',
+  url = 'https://github.com/MS047/CellTypeClassifier', # use the URL to the github repo
+  download_url = 'https://github.com/MS047/CellTypeClassifier/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['Kilosort', 'Phy', 'Classifier', 'sklearn', 'Extracellular', 'Spike Sorting'], # arbitrary keywords
+  classifiers = [],
+)
